c6bd7bfba0510a86a80e1b0a1665a2e3172e5485
katsuragawa <andre.katsuragawa@mercadolivre.com>
2022-11-11 18:38:23 -0300

    migrate to next

---
+++ app/provas/page.tsx
@@ -0,0 +1,13 @@
+export default function Tests() {
+  return (
+    <div className="p-8">
+      <div className="flex items-center justify-between font-bold">
+        <h1 className="text-5xl">Provas</h1>
+        <div className="flex items-center gap-12">
+          <div>Conectar aplicativo</div>
+          <div className="rounded-full bg-blue-900 px-8 py-2">Adicionar questões</div>
+        </div>
+      </div>
+    </div>
+  );
+}
